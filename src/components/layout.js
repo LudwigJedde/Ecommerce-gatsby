d2247c9a3509db8e5a6853406ba7e1e5c95ec527
unknown <ludwigjedde.pro@gmail.com>
2019-08-16 10:33:25 +0200

    Contruction des pages + connection Contentful

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-/*import { useStaticQuery, graphql } from "gatsby";*/
-import "./bootstrap.min.css";
-import "./layout.css";
-
 import Navbar from "./Globals/Navbar";
-const Layout = ({ children }) => <>
-<Navbar/>
-{children}</>;
+import Footer from "./Globals/Footer";
+import "./layout.css";
+import "./bootstrap.min.css";
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+    <Footer />
+  </>
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 };
 
 export default Layout;
